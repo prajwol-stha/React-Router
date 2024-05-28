d54aa6f45f2e1b43bbd108ccebcfc6b749813783
Prajwol Shrestha <prajwol.stha777@gmail.com>
2024-05-28 20:05:35 +0545

    Home, header, footer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import{Link,NavLink} from 'react-router-dom'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-600 p-4'>React Router</h1>
+      
+    </>
+  )
+}
+
+export default App
